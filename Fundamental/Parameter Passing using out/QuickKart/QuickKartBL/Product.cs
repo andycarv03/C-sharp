fb3f0e0acc577cff151a6f1ff13d650f6b3b8c18
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:01:37 +0200

    Add files via upload

---
+++ Fundamental/Parameter Passing using out/QuickKart/QuickKartBL/Product.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Product
+    {
+        //Auto-implemented properties
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public byte CategoryId { get; set; }
+        public double Price { get; set; }
+        public int QuantityAvailabe { get; set; }
+
+        public Product(string productId, string productName, byte categoryId, double price, int quantityAvailable)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            CategoryId = categoryId;        
+            Price = price;
+            QuantityAvailabe = quantityAvailable;
+        }
+
+        public string CalculateDiscount(ref int discount, out double minPrice, out double maxPrice)
+        {
+            string result = "Eligible for discount";
+
+            if (this.Price <= 500)
+            {
+                minPrice = 1;
+                maxPrice = 500;
+                result = "Not eligible for discount";
+            }
+            else if (this.Price > 500 && this.Price <= 1000)
+            {
+                discount = 5;
+                minPrice = 501;
+                maxPrice = 1000;
+            }
+            else if (this.Price > 1000 && this.Price <= 5000)
+            {
+                discount = 10;
+                minPrice = 1001;
+                maxPrice = 5000;
+            }
+            else if (this.Price > 5000 && this.Price <= 10000)
+            {
+                discount = 15;
+                minPrice = 5001;
+                maxPrice = 10000;
+            }
+            else
+            {
+                minPrice = 10001;
+                maxPrice = double.MaxValue;
+                discount = 20;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Fundamental/Parameter Passing using out/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productOne = new Product("P131", "Marble chess board", 1, 1099.99, 10);
+            
+            int discount = 0;
+            double minValue;
+            double maxValue;
+
+            //use out parameters in the method to return more than one value from the method
+            string result = productOne.CalculateDiscount(ref discount, out minValue, out maxValue);
+
+            Console.WriteLine(result);
+            Console.WriteLine("Discount for the product = {0}", discount);
+            Console.WriteLine("Minimum Price = {0}", minValue);
+            Console.WriteLine("Minimum Price = {0}", maxValue);
+        }
+
+        /*
+         * ref variable must be initialised before passing to called method.
+         * Value can be modified before return within called method.
+         * 
+         * out variable may not be initialised before the called method.
+         * must be initialised before return within the called method.
+         */
+    }
+}
