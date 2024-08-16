593bbfbe03c36d63a40ceb54141c6bf8ac3f94f8
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:27:57 +0200

    Add files via upload

---
+++ Advanced/Built-In Delegates - Func/QuickKart/QuickKartBL/Sale.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Sale
+    {
+        public Customer Customer { get; set; }
+        public int Discount { get; set; }
+
+        public Sale(Customer customer)
+        {
+            this.Customer = customer;
+        }
+        public string YearEndSale()
+        {
+            this.Discount = 15;
+            string message = string.Empty;
+
+            #region Single Alert Preference
+            //if (Customer.AlertPreference == "SMS")
+            //{
+            //    message = Alerts.SendSMS(Customer.CustomerName, Customer.PhoneNumber, Discount);
+            //}
+            //if (Customer.AlertPreference == "Email")
+            //{
+            //    message = Alerts.SendEmail(Customer.CustomerName, Customer.EmailId, Discount);
+            //}
+            //if (Customer.AlertPreference == "WhatsApp")
+            //{
+            //    message = Alerts.SendWhatsApp(Customer.CustomerName, Customer.PhoneNumber, Discount);
+            //}
+            #endregion
+
+            #region Multiple Alert Preferences
+
+            //foreach (var item in Customer.AlertPreferences)
+
+            //{
+            //    if (item == "SMS")
+            //    {
+            //        message += Alerts.SendSMS(Customer.CustomerName, Customer.PhoneNumber, Discount);
+            //    }
+            //    if (item == "Email")
+            //    {
+            //        message += Alerts.SendEmail(Customer.CustomerName, Customer.EmailId, Discount);
+            //    }
+            //    if (item == "WhatsApp")
+            //    {
+            //        message += Alerts.SendWhatsApp(Customer.CustomerName, Customer.PhoneNumber, Discount);
+            //    }
+            //}
+
+            message = Customer.AlertPreferences(Customer.CustomerName, Customer.PhoneNumber, Discount);
+
+            #endregion
+
+            return message;
+        }
+
+        public string ClearanceSale()
+        {
+            return null;
+        }
+
+        public string FestiveOffer()
+        {
+            return null;
+        }
+
+        public static double BirthdayDiscount(double amount)
+        {
+            double discountAmount = amount * 0.2;
+            return discountAmount;
+        }
+    }
+}
---
+++ Advanced/Built-In Delegates - Func/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickKartBL;
+
+
+namespace QuickKartTestApp
+{
+    //delegate having return type as double and accepting a double parameter
+    //Explicit declaration not required if Func is used
+    //public delegate double BirthdayDelegate(double amount);
+
+    //second delegate
+    //Explicit declaration not required if Func is used
+    //public delegate bool CheckPriceDelegate(Product product);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BirthdayDelegate discountDel = x => x * 0.2;
+
+            //BirthdayDelegate discountDel = x => Sale.BirthdayDiscount(x);
+            Func<double, double> discountDel = x => Sale.BirthdayDiscount(x);
+
+            double discountCustomerOne = discountDel(2500);
+            Console.WriteLine("Amount after applying 20% discount = " + discountCustomerOne);
+
+            double discountCustomerTwo = discountDel(5600);
+            Console.WriteLine("Amount after applying 20% discount = " + discountCustomerTwo);
+
+            double discountCustomerThree = discountDel(4000);
+            Console.WriteLine("Amount after applying 20% discount = " + discountCustomerThree);
+
+            //creating a second delegate instance
+            //CheckPriceDelegate checkPrice = p => p.Price > 0;
+
+            //Func taking Product as input and bool as output
+            Func<Product, bool> checkPrice = p => p.Price > 0;
+
+            Console.WriteLine();
+
+            Product product = new Product("BasketBall", 1500, 6, 1);
+            bool result = checkPrice(product);
+
+            Console.WriteLine("Result = {0}", result);
+
+        }
+    }
+}
