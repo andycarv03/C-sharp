a12f0df859ff38bf88185c98e610f689c9ca3a3c
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:00:19 +0200

    Add files via upload

---
+++ Fundamental/Need For and Create Properties/QuickKart/QuickKartBL/Category.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Category
+    {
+        //Instance variables without access modifiers are by default private to the class.
+        private byte categoryId;
+        private string categoryName;
+
+        //Properties with validation in the business logic layer. Protect the variables from being set to null/0
+        public byte CategoryId
+        {
+            get
+            {
+                return categoryId;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    categoryId = value;
+                }
+            }
+        }
+
+        public string CategoryName
+        {
+            get
+            {
+                return categoryName;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    categoryName = value;
+                }
+                else
+                {
+                    categoryName = "Unassigned";
+                }
+            }
+        }
+        public Category(byte categoryId, string categoryName)
+        {
+            this.categoryId = categoryId;
+            this.categoryName = categoryName;
+        }
+    }
+
+}
---
+++ Fundamental/Need For and Create Properties/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QuickKartBL;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Category sportsCategory = new Category(0, null);
+        Console.WriteLine("CategoryId = {0}", sportsCategory.CategoryId);
+        Console.WriteLine("CategoryName = {0}\n", sportsCategory.CategoryName);
+
+        sportsCategory.CategoryId = 100;
+        sportsCategory.CategoryName = "Tennis";
+
+        Console.WriteLine("CategoryId = {0}", sportsCategory.CategoryId);
+        Console.WriteLine("CategoryName = {0}", sportsCategory.CategoryName);
+    }
+}
