3ef3ebfe3ef5712d6083b2067fc94a551c803c59
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 19:45:51 +0200

    Add files via upload

---
+++ Fundamental/Abstract Class and Methods/QuickKart/QuickKartBL/DomesticSeller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class DomesticSeller:Seller
+    {
+        public bool ExpressDelivery { get; set; }
+
+        public DomesticSeller(string sellerId, string sellerName, 
+                              string[] sellerLocations, bool expressDelivery)
+                              : base(sellerId,sellerName,sellerLocations)
+        {
+            this.ExpressDelivery = expressDelivery;
+        }
+
+        public override double CalculateShippingCharges(string destination)
+        {
+            double shippingCharges = 0;
+            if (SellerLocations.Contains(destination))
+            {
+                if (destination.Equals("New York") || destination.Equals("Chicago"))
+                {
+                    shippingCharges = 50;
+                }
+                else
+                {
+                    shippingCharges = 100;
+                }
+                if (ExpressDelivery)
+                {
+                    shippingCharges += 250;
+                }
+            }
+            else
+            {
+                shippingCharges = 0;
+            }
+            return shippingCharges;
+        }
+    }
+}
---
+++ Fundamental/Abstract Class and Methods/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using QuickKartBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seller sellerOne = 
+                new DomesticSeller("S1001", 
+                "David", 
+                new[] { "New York", "Raleigh" }, 
+                true);
+            Console.WriteLine("New York : " 
+                + sellerOne.CalculateShippingCharges("New York"));
+            Console.WriteLine("Raleigh : " 
+                + sellerOne.CalculateShippingCharges("Raleigh"));
+            Console.WriteLine("Chicago : " 
+                + sellerOne.CalculateShippingCharges("Chicago"));
+
+            Console.WriteLine("----------------------");
+            string details = sellerOne.GetSellerInformation();
+            Console.WriteLine(details);
+
+
+            //Error. cannot instantiate an abstract class. It can be used only to store the reference of a derived class object similar to dynamic polymorphism.
+            //Seller seller = new Seller();
+        }
+    }
+}
