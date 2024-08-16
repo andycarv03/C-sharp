4d2353b43f59f7420252b3aef6e79116b13a0b2c
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:11:46 +0200

    Add files via upload

---
+++ Fundamental/Static Variables and Constructor/QuickKart/QuickKartBL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Customer
+    {
+        /* Auto-implemented properties. NO private variables and NO logic within the accessor methods.
+         * Compiler will set the value to zero/null if no value passed to the object.
+         */
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        static private int counter;                     //We can also intialise static variable in a signle-line
+
+        static Customer()                               //static constructor executed one time before the instantiation of the first object of the class
+        {
+            //Initialise static variables
+            counter = 1000;
+        }
+
+        public Customer()
+        {
+            //counter = 1000;                           //Initialising static variable within a class contructor will make it a variable local to the instantiated object
+            counter = counter + 1;
+            CustomerId = counter;
+
+        }
+    }
+}
---
+++ Fundamental/Static Variables and Constructor/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customerOne = new Customer();
+            Customer customerTwo = new Customer();
+            Customer customerThree = new Customer();
+
+            Console.WriteLine("CustomerOne ID = {0}", customerOne.CustomerId);
+            Console.WriteLine("CustomerTwo ID = {0}", customerTwo.CustomerId);
+            Console.WriteLine("CustomerThree ID = {0}", customerThree.CustomerId);
+        }
+    }
+}
