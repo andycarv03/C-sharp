133d51eda675f41852739d09cbbb04675bec701a
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:20:16 +0200

    Add files via upload

---
+++ Fundamental/XUNIT/QuickKartBL/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public byte CategoryId { get; set; }
+
+        private double price;       
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value < 0)
+                    this.price = 0;
+                else
+                    this.price = value;
+            }
+        }
+        public int QuantityAvailable { get; set; }
+
+        public Product(string productId, string productName, byte categoryId, double price, int quantityAvailable)
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.CategoryId = categoryId;
+            this.Price = price;
+            this.QuantityAvailable = quantityAvailable;
+        }
+
+        public bool CheckAvailability(int quantityToPurchase)
+        {
+            bool availability = false;
+
+            if (quantityToPurchase <= this.QuantityAvailable)
+                availability = true;
+
+            return availability;
+        }
+    }
+}
---
+++ Fundamental/XUNIT/QuickKartTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productObj = new Product("P101", "Samsung Galaxy S4", 1, 38000.00, 10);
+
+            Console.WriteLine("ProductId = {0}\nProductName = {1}\nCategoryId = {2}\n" +
+                "Price = {3}\nQuantityAvailable = {4}", productObj.ProductId, productObj.ProductName,
+                productObj.CategoryId, productObj.Price, productObj.QuantityAvailable);
+
+            int quantityToPurchase = 2;
+
+            Console.WriteLine("--------------------------------\n");
+            if (productObj.CheckAvailability(quantityToPurchase))
+            {
+                Console.WriteLine("Product is IN STOCK!");
+            }
+            else
+            {
+                Console.WriteLine("Product is OUT OF STOCK!!");
+            }
+            Console.WriteLine("\n--------------------------------");
+        }
+    }
+}
