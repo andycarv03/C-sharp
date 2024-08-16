ec8fd83f31c1897ad0bdd1aed5b59d5fdce70ab0
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:10:43 +0200

    Add files via upload

---
+++ Fundamental/Static Methods/QuickKart/QuickKartBL/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Customer
+    {
+        //Auto-implemented properties.
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //static variable declaration
+        private static int counter;
+
+        //static variable initialised in static constructor
+        static Customer()
+        {
+            counter = 1000;
+        }
+        
+        //parameterless constructor
+        public Customer()
+        {
+            CustomerId = ++counter;
+        }
+
+        public static int GetTotalNumberOfCustomer()
+        {
+            int TotalCustomer = counter - 1000;
+            return TotalCustomer;
+        }
+    }
+}
---
+++ Fundamental/Static Methods/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customerOne = new Customer();
+            Customer customerTwo = new Customer();
+            Customer customerThree = new Customer();
+
+            Console.WriteLine("CustomerOne ID = {0}", customerOne.CustomerId);
+            Console.WriteLine("CustomerTwo ID = {0}", customerTwo.CustomerId);
+            Console.WriteLine("CustomerThree ID = {0}", customerThree.CustomerId);
+
+            /*
+             * static methods are used to access static variables by ClassName.MethodName()
+             * static methods cannot be invoked by pbject reference.
+             * static methods/variables belong to a class.
+             * static method can only access static data. it cannot access non-static data.
+             */
+
+            Console.WriteLine("Total number of Customers = {0}", Customer.GetTotalNumberOfCustomer());
+        }
+    }
+}
