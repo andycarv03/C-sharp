967cd08c674c0d74fca9559ecb22aeaed3e83f03
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:24:19 +0200

    Add files via upload

---
+++ Advanced/Applying Single Responsibility Principle/Applying Single Responsibility Principle/QuickKart/QuickKartBL/PersistCustomerDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class PersistCustomerDetails
+    {
+        public Customer Customer { get; set; }
+
+        public PersistCustomerDetails(Customer customer) 
+        {
+            this.Customer = customer;
+        }
+
+        public string SaveCustomerDetails()
+        {
+            string path = @"C:\Users\andyc\Downloads\CustomerDetails.txt";
+            FileStream fileStream = null;
+            bool result = File.Exists(path);
+
+            if (result)
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Write);
+            else
+                fileStream = new FileStream(path, FileMode.CreateNew, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.WriteLine("{0}, {1}, {2}, {3}, {4}",
+                Customer.CustomerId, Customer.CustomerName, Customer.ContactNumber, Customer.EmailId, Customer.Address);
+            writer.Close();
+            return "\nDetails SAVED successfully in a file!\n";
+        }
+
+
+    }
+}
---
+++ Advanced/Applying Single Responsibility Principle/Applying Single Responsibility Principle/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using QuickKartBL;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("C101", "Jenny", "9845054609", "jenny@gmail.com",
+                "Maple street, 5th cross road");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("           Customer Details           ");
+            Console.WriteLine("--------------------------------------");
+            string customerDetails = customer.GetCustomerDetails();
+            Console.WriteLine(customerDetails);
+
+            string updateStatus = customer.UpdateContactDetails("9995094609",
+                "jenny123@gmail.com", null);
+            Console.WriteLine(updateStatus);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("       Updated Customer Details       ");
+            Console.WriteLine("--------------------------------------");
+            customerDetails = customer.GetCustomerDetails();
+            Console.WriteLine(customerDetails);
+
+            PersistCustomerDetails persistDetails = new PersistCustomerDetails(customer);
+            string saveStatus = persistDetails.SaveCustomerDetails();
+            Console.WriteLine(saveStatus);
+        }
+    }
+}
