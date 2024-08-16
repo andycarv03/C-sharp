d1461ceadb7152623c75a50efea2ce5ac2dd59ec
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:04:56 +0200

    Add files via upload

---
+++ Fundamental/Parameter Passing using ref/QuickKart/QuickKartBL/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Product
+    {
+        //Auto-implemented properties
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public byte CategoryId { get; set; }
+        public double Price { get; set; }
+        public int QuantityAvailabe { get; set; }
+
+        public Product(string productId, string productName, byte categoryId, double price, int quantityAvailable)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            CategoryId = categoryId;        
+            Price = price;
+            QuantityAvailabe = quantityAvailable;
+        }
+
+        public string CalculateDiscount(ref int discount)
+        {
+            string result = "Eligible for discount";
+
+            if (this.Price <= 500)
+            {
+                result = "Not eligible for discount";
+            }
+            else if (this.Price > 500 && this.Price <= 1000)
+            {
+                discount = 5;
+            }
+            else if (this.Price > 1000 && this.Price <= 5000)
+            {
+                discount = 10;
+            }
+            else if (this.Price > 5000 && this.Price <= 10000)
+            {
+                discount = 15;
+            }
+            else
+            {
+                discount = 20;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Fundamental/Parameter Passing using ref/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productOne = new Product("P131", "Marble chess board", 1, 1099.99, 10);
+            
+            int discount = 0;           //necessary to initialise before passing discount as a reference variable
+
+            /*
+             * Variables passed, should be initialized before the method call 
+             * ref keyword must be specified both in the method call and in the method definition
+             * A method can have any number of ref parameters
+             */
+            string result = productOne.CalculateDiscount(ref discount);
+
+            Console.WriteLine(result);
+            Console.WriteLine("Discount for the product = {0}", discount);
+        }
+    }
+}
