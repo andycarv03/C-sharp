4e5c2f702b40f65a9dc7ed45181ab89da13b5d73
andycarv03 <42095967+andycarv03@users.noreply.github.com>
2024-08-16 20:02:44 +0200

    Add files via upload

---
+++ Fundamental/Parameter Passing using params/QuickKart/QuickKartBL/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string EmailId { get; set; }
+        public long ContactNumber { get; set; }
+        public string ContactAddress { get; set; }
+        public string[] DeliveryAddress { get; set; }               //to store n number of addresses
+
+        public Customer(int customerId, string customerName, string emailId, long contactNumber, string contactAddress)
+        {
+            this.CustomerId = customerId;
+            this.CustomerName = customerName;
+            this.EmailId = emailId;
+            this.ContactNumber = contactNumber;
+            this.ContactAddress = contactAddress;
+        }
+
+        public string AddDeliveryAddress(params string[] deliveryAddress)
+        {
+            this.DeliveryAddress = deliveryAddress;
+            return "Delivery adddress(es) added successfuly!";
+        }
+    }
+}
---
+++ Fundamental/Parameter Passing using params/QuickKart/QuickKartTestApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using QuickKartBL;
+using System;
+
+namespace QuickKartTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customerOne = new Customer(1001, "Taylor Swift", "taylor@gmail.com", 9807865432, "723, Amsterdam");
+            Customer customerTwo = new Customer(1002, "Harry Styles", "harry@gmail.com", 8679321789, "675, Italy");
+            Customer customerThree = new Customer(1003, "Justin Bieber", "justin@gmail.com", 9909311780, "301, Newark");
+
+            //use params keyword for passing variable number of parameters to a method and implement the above mentioned requirement.
+            /* Req : According to the requirement, there should be more properties (or fields) required to store different delivery addresses.
+             * But there is only one ContactAddress, to store the customer contact address.
+             */
+
+            //This just increases the code length. Instead you can use these addresses passed as parameters directly to the method.
+
+            //string[] DeliveryAddressOne = new string[] { "Netherlands" };
+            //customerOne.AddDeliveryAddress(DeliveryAddressOne);
+
+            //string[] DeliveryAddressTwo = new string[] { "Netherlands" , "Italy" , "Switzerland"};
+            //customerTwo.AddDeliveryAddress(DeliveryAddressTwo);
+
+            //string[] DeliveryAddressThree = new string[] { "Netherlands", "Italy", "Switzerland" , "Greenland", "Norway", "Prague"};
+            //customerThree.AddDeliveryAddress(DeliveryAddressThree);
+
+            string DeliveryAddresses = customerOne.AddDeliveryAddress("Netherlands");
+            Console.WriteLine(DeliveryAddresses);
+
+            DeliveryAddresses = customerTwo.AddDeliveryAddress("Netherlands", "Italy", "Switzerland");
+            Console.WriteLine(DeliveryAddresses);
+
+            DeliveryAddresses = customerThree.AddDeliveryAddress("Netherlands", "Italy", "Switzerland", "Greenland", "Norway", "Prague");
+            Console.WriteLine(DeliveryAddresses);
+
+            foreach (string item in customerThree.DeliveryAddress)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            /*
+             * 'params' is always used with an array to hold variable length of parameter values 
+             * If you do not send any arguments, the length of the params list will be zero
+             * No additional parameters are permitted after the params keyword in the method declaration
+             * Only one params keyword is permitted in a method declaration 
+             */
+        }
+    }
+}
